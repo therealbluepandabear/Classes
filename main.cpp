e7ab3612a53c807f7075935251dee3166d22a26c
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 16:07:48 +1300

    Added some public functions (behaviours of the Car class).

--- main.cpp
+++ main.cpp
@@ -4,8 +4,13 @@ private:
     float fuel;
     float speed;
     int passengers;
+public:
+    void FillFuel(float amount);
+    void Accelerate();
+    void Brake();
+    void AddPassengers();
+    void Dashboard();
 };
-
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
