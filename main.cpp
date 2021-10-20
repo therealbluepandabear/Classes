eb1eabc06f2ddc5c7d0bf26cd77f6dc1e39eef7a
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 16:08:54 +1300

    Generated function definitions.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,26 @@ public:
     void Dashboard();
 };
 
+void Car::FillFuel(float amount) {
+
+}
+
+void Car::Accelerate() {
+
+}
+
+void Car::Brake() {
+
+}
+
+void Car::AddPassengers() {
+
+}
+
+void Car::Dashboard() {
+
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
